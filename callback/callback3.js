127d251e9cf724491f57abe7b603061142a52cab
Shohan494 <shohan12224943@gmail.com>
2017-06-06 16:24:44 +0600

    folder names are edited and callback basic

---
+++ callback/callback3.js
@@ -0,0 +1,8 @@
+	//Global variable​
+	​var generalLastName = "Clinton";
+	​
+	​function getInput (options, callback) {
+	    allUserData.push (options);
+	​// Pass the global variable generalLastName to the callback function​
+	    callback (generalLastName, options);
+	}
