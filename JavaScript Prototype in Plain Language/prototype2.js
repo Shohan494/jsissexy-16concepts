7f5518c52e3c8b3e4d91ff3ba546c2d318140b0f
Shohan494 <shohan12224943@gmail.com>
2017-06-06 11:58:34 +0600

    basic prototype and inheritence demonstration

---
+++ JavaScript Prototype in Plain Language/prototype2.js
@@ -0,0 +1,14 @@
+	function Account () {
+	}
+	// This is the use of the Account constructor to create the userAccount object.​
+	var userAccount = new Account (); 
+
+	//The constructor in this example is Object () 
+	var myObj = new Object ();
+	// And if you later want to find the myObj constructor:
+	console.log(myObj.constructor); // Object()
+	
+	// Another example: Account () is the constructor
+	var userAccount = new Account (); 
+	// Find the userAccount object's constructor
+	console.log(userAccount.constructor); // Account()
